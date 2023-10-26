737940adcbd980b1d7c526aa3a3c9bf878909df0
lzapataa <luisharold.zapataarismendi@everis.nttdata.com>
2023-10-26 12:15:20 -0500

    Modificacion clase EjemploCelula, adicion de depencias en pom.xml

--- src/main/java/com/microservicios/demo/celula/EjemploCelula.java
+++ src/main/java/com/microservicios/demo/celula/EjemploCelula.java
@@ -1,23 +1,20 @@
 package com.microservicios.demo.celula;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class EjemploCelula {
 
     public static void main(String[] args) {
         SpringApplication.run(EjemploCelula.class, args);
-        System.out.println(SpringApplication.run(EjemploCelula.class, args);
-        System
+        System.out.println(SpringApplication.run(EjemploCelula.class, args));
+
 
         SpringApplication.run(EjemploCelula.class, args);
 
 
-        SpringApplication.run(HolaMundo
 
-                SpringApplication.run(H
-
-                        SpringApplication
-                        "Hola Mundo");
     }
 }
-}
+
