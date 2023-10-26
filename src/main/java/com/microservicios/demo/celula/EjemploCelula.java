acaae388aac44ee4d19fed313a5b557e33c8e2f8
aballeng <aballeng@emeal.nttdata.com>
2023-10-26 15:16:09 -0500

    Ejemplo error git

--- src/main/java/com/microservicios/demo/celula/EjemploCelula.java
+++ src/main/java/com/microservicios/demo/celula/EjemploCelula.java
@@ -1,20 +1,14 @@
 package com.microservicios.demo.celula;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EjemploCelula {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EjemploCelula.class, args);
-        System.out.println(SpringApplication.run(EjemploCelula.class, args));
-
-
-        SpringApplication.run(EjemploCelula.class, args);
-
-
-
+    @GetMapping("/saludar")
+    public String saludar() {
+        return "Hola Mundo Alejandro";
     }
 }
 
