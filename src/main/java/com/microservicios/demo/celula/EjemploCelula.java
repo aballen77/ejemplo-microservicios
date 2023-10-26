ca676e1c2f1cfd57fc8d0214e06638902d11e790
aballeng <aballeng@emeal.nttdata.com>
2023-10-26 11:53:35 -0500

    Creacion Archivos Principales

---
+++ src/main/java/com/microservicios/demo/celula/EjemploCelula.java
@@ -0,0 +1,23 @@
+package com.microservicios.demo.celula;
+
+import org.springframework.boot.SpringApplication;
+@RestController
+public class EjemploCelula {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploCelula.class, args);
+        System.out.println(SpringApplication.run(EjemploCelula.class, args);
+        System
+
+        SpringApplication.run(EjemploCelula.class, args);
+
+
+        SpringApplication.run(HolaMundo
+
+                SpringApplication.run(H
+
+                        SpringApplication
+                        "Hola Mundo");
+    }
+}
+}
